af099459fd2833e053a87ad4bd5281139ec7c046
MaximoNorry <maxiurcmonaco@gmail.com>
2025-10-05 01:20:59 -0300

    añado el productos.js y el productoscontroller.js

---
+++ backend/data/productos.js
@@ -0,0 +1,8 @@
+const productos = [
+{ id: 1, nombre: "Aparador Uspallata", precio: 250000, descripcion: "SAparador de seis puertas.", imagen: "" },
+{ id: 2, nombre: "Biblioteca Recoleta", precio: 180000, descripcion: "Biblioteca Recoleta.", imagen: "" },
+{ id: 3, nombre: "Butaca Mendoza", precio: 60000, descripcion: "Butaca Mendoza.", imagen: "" },
+{ id: 4, nombre: "Sillón Copacabana", precio: 150000, descripcion: "Sillón Copacabana", imagen: "" }
+];
+
+module.exports = productos;
