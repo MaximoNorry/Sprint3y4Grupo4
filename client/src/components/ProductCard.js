90234bb1a7f4d59906de5bd0d69157cc13a7443d
johanngraneros <granerosjohann05@gmail.com>
2025-10-06 01:20:22 -0300

    Agrego producto.precio

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -6,6 +6,7 @@ return (
     <img src={`/images/${producto.nombre.toLowerCase().replace(/ /g, " ")}.png`} alt={producto.nombre} />
     <h3>{producto.nombre}</h3>
     <p>{producto.descripcion}</p>
+    <p>${producto.precio}</p>
     </div>
 );
 };
