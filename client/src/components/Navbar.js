a875516c69a7ae23d21f6b9e8ed094bde05d2314
johanngraneros <granerosjohann05@gmail.com>
2025-10-06 01:18:27 -0300

    Modifico NavBar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,38 +1,37 @@
 import React from "react";
 
-function Navbar({ carritoCount, volverAlCatalogo }) {
-return (
+function Navbar({ carritoCount, volverAlCatalogo, toggleCarrito }) {
+  return (
     <header>
-        <div className="header-container">
-            <div
-                className="logo-izquierda"
-                onClick={volverAlCatalogo}
-                style={{ cursor: "pointer", display: "flex", alignItems: "center" }}
-            >
-                <a href="../public/index.html">
-                    <img
-                        src="../public/images/logo.svg"
-                        alt="Logo hermanos jota"
-                        className="logo"
-                    />
-                </a>
-                <h1 className="titulo-centro" href="../public/index.html" >Hermanos Jota</h1>
-            </div>
-            <nav>
-                <ul>
-                    <li><a href="index.html">Inicio</a></li>
-                    <li><a href="/catalogo">Catálogo</a></li>
-                    <li><a href="/contacto">Contacto</a></li>
-                    <li>
-                        <a className="carrito" type="button" href="">
-                            🛒 Carrito (<span id="contador-carrito">{carritoCount}</span>)
-                        </a>
-                    </li>
-                </ul>
-            </nav>
+      <div className="header-container">
+        <div
+          className="logo-izquierda"
+          onClick={volverAlCatalogo}
+          style={{ cursor: "pointer", display: "flex", alignItems: "center" }}
+        >
+          <img
+            src="/images/logo.svg"
+            alt="Logo Hermanos Jota"
+            className="logo"
+          />
+          <h1 className="titulo-centro">Hermanos Jota</h1>
         </div>
+
+        <nav>
+          <ul>
+            <li><a href="/">Inicio</a></li>
+            <li><a href="/catalogo">Catálogo</a></li>
+            <li><a href="/contacto">Contacto</a></li>
+            <li>
+              <button className="carrito" onClick={toggleCarrito}>
+                🛒 Carrito (<span className="carrito-badge">{carritoCount}</span>)
+              </button>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
-);
+  );
 }
 
-export default Navbar;
+export default Navbar
